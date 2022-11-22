852c5b3a162d4c1a7bacec67dc11a9f723a29871
charles-bruel <csbruel@gmail.com>
2022-11-21 19:56:08 -0500

    Added magic carpet code

---
+++ custom/VarietyPack/SegmentCarpet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using PrecompiledExtensions;
+
+namespace VarietyPack
+{
+    public class SegmentCarpet : LiftTowerCablePathFetcher
+    {
+        private Transform pivot;
+
+        public override void OnParameterUpdate(Transform prevTower, Transform nextTower, Transform currentTowerPos)
+        {
+            if(pivot == null)
+            {
+                pivot = transform.GetChild(0);
+            }
+            Vector3 delta = nextTower.position - currentTowerPos.position;
+            float len = delta.magnitude;
+            float y = delta.y;
+            delta.y = 0;
+            float x = delta.magnitude;
+            float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+            pivot.localEulerAngles = new Vector3(angle, -90, 0);
+            pivot.localScale = new Vector3(0.5f, 1, len);
+        }
+    }
+}
---
+++ custom/VarietyPack/TowerPlacerCarpet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using PrecompiledExtensions;
+
+namespace VarietyPack
+{
+    class TowerPlacerCarpet : TowerPlacementAlgorithm
+    {
+        public override void PlaceTowers(float[][] constructionConstraintsData, List<Vector3> terrainPositions, List<int> towerIndices)
+        {
+            towerIndices.Capacity = terrainPositions.Count;
+            for(int i = 0;i < terrainPositions.Count;i ++)
+            {
+                towerIndices.Add(i);
+            }
+        }
+    }
+}
